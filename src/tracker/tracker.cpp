f5d4da89e289ad22fc2dabd9cc901731c24d89f2
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-02 23:01:59 +0530

    Parse and use tracker_info.txt

--- src/tracker/tracker.cpp
+++ src/tracker/tracker.cpp
@@ -1,17 +1,65 @@
 /**
  * @file tracker.cpp
- * @brief The entrypoint to the tracker.
+ * @brief The tracker.
+ *
+ * @details
+ * Usage:
+ *
+ * ```
+ * ./tracker tracker_info.txt <tracker_no>
+ * ```
+ *
+ * tracker info file contains a list of tracker IPs and ports
+ * tracker_no is the index of the tracker to use (1 or 2)
+ *
  * @author Swastik Pal
  * @date 2024-10-1
  *
  * @copyright Copyright (c) 2024
  */
 
+#include <cstring>
 #include <iostream>
+#include <fcntl.h>
 #include <memory>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <thread>
 
 #include "../network/tcp_server.hpp"
+
+static std::string ip;
+static uint16_t port = 0;
+
+void load_tracker_info(const char *file, int n)
+{
+	int fd = open(file, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		std::cerr << "Error opening file" << strerror(errno) << std::endl;
+		exit(1);
+	}
+	constexpr size_t len = 1024;
+	char contents[len];
+	ssize_t valread = read(fd, contents, len); // read the file in one shot, assuming it's small
+	close(fd);
+	if (valread == -1)
+	{
+		std::cerr << "Error reading file" << strerror(errno) << std::endl;
+		exit(1);
+	}
+	int i = 1;
+	char *line = contents;
+	while (i <= n)
+	{
+		char buffer[len];
+		sscanf(line, "%s %hu", buffer, &port);
+		line = strchr(line, '\n') + 1;
+		ip = buffer;
+		++i;
+	}
+	std::cout << "IP: " << ip << " Port: " << port << std::endl;
+}
 
 void loop()
 {
@@ -32,7 +80,7 @@ void loop()
 	server.setOnConnect(onConnect);
 	server.setOnDisconnect(onDisconnect);
 	server.setOnData(onData);
-	server.start(0); // port 0 means the OS will choose a random port
+	server.start(port);
 
 	while (true)
 	{
@@ -54,12 +102,23 @@ void loop()
 	server.stop();
 }
 
-/**
- * @brief Execution starts here
- */
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello from tracker!" << std::endl;
+	// Parse args
+	if (argc != 3)
+	{
+		std::cerr << "Usage: ./tracker tracker_info.txt <tracker_no>" << std::endl;
+		exit(1);
+	}
+	int n = std::stoi(argv[2]);
+	// Sanitize input
+	if (n != 1 && n != 2)
+	{
+		std::cerr << "Invalid tracker number" << std::endl;
+		exit(1);
+	}
+
+	load_tracker_info(argv[1], n);
 	loop();
 	return 0;
 }
