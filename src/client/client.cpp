1f57fcd94f68af10fff8077e2b5b3b1db87479de
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-02 19:38:58 +0530

    Server test
    Send message from 10000 clients.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -9,11 +9,24 @@
 
 #include <iostream>
 
+#include "../network/tcp_socket.hpp"
+
 /**
  * @brief Execution starts here
  */
 int main()
 {
-	std::cout << "Hello from client!" << std::endl;
+	uint16_t port;
+	std::cout << "Enter the port number: ";
+	std::cin >> port;
+	const int n = 10000;
+	// Create n sockets and connect to the server
+	for (int i = 0; i < n; i++)
+	{
+		TCPSocket socket;
+		socket.connect("127.0.0.1", port);
+		std::string message = "Hello from client " + std::to_string(i);
+		socket.send_data(message);
+	}
 	return 0;
 }
