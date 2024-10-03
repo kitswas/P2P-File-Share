2fd59d719ae8155930f36d6aaed759763093f947
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-03 21:18:14 +0530

    Command create_group

--- src/tracker/process_request.cpp
+++ src/tracker/process_request.cpp
@@ -1,10 +1,18 @@
 #include "process_request.hpp"
+#include "groupdb.hpp"
 #include "userdb.hpp"
 
 #include <sstream>
 
+static GroupDB groupDB;
+
 static std::unordered_map<std::shared_ptr<TCPSocket>, std::shared_ptr<User>> logged_in_users;
 static UserDB userDB;
+
+bool is_logged_in(std::shared_ptr<TCPSocket> client)
+{
+	return logged_in_users.find(client) != logged_in_users.end();
+}
 
 bool process_user_request(std::shared_ptr<TCPSocket> client, std::string_view request, std::stringstream &datastream)
 {
@@ -56,7 +64,7 @@ bool process_user_request(std::shared_ptr<TCPSocket> client, std::string_view re
 		std::string password;
 		datastream >> username >> password;
 		auto user = userDB.getUser(username);
-		if (user && logged_in_users.find(client) == logged_in_users.end() && user->checkPassword(password))
+		if (user && !is_logged_in(client) && user->checkPassword(password))
 		{
 			logged_in_users.try_emplace(client, user);
 			client->send_data("Login successful\n");
@@ -84,12 +92,44 @@ bool process_user_request(std::shared_ptr<TCPSocket> client, std::string_view re
 	return true;
 }
 
+bool process_group_request(std::shared_ptr<TCPSocket> client, std::string_view request, std::stringstream &datastream)
+{
+	const char *const login_required_warning = "You must be logged in to perform this action\n";
+	if (request == "create_group")
+	{
+		group_id_t group_id;
+		datastream >> group_id;
+		std::shared_ptr<User> owner = nullptr;
+		try
+		{
+			owner = logged_in_users.at(client);
+			if (groupDB.createGroup(group_id, owner))
+			{
+				client->send_data("Group created\n");
+			}
+			else
+			{
+				client->send_data("Group already exists\n");
+			}
+		}
+		catch (std::out_of_range &_)
+		{
+			client->send_data(login_required_warning);
+		}
+	}
+	else
+	{
+		return false;
+	}
+	return true;
+}
+
 void process_request(std::shared_ptr<TCPSocket> client, std::string &data)
 {
 	std::stringstream ss(data);
 	std::string request;
 	ss >> request;
-	if (!process_user_request(client, request, ss))
+	if (!process_user_request(client, request, ss) && !process_group_request(client, request, ss))
 	{
 		client->send_data("Invalid request\n");
 	}
