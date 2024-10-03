10fceb456a5563fa5c6f49eb458be7b5a7815718
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-03 15:18:22 +0530

    Automatic error recovery on disconnect

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -14,12 +14,18 @@
 #include "../network/network_errors.hpp"
 #include "../network/tcp_socket.hpp"
 
-static std::string ip;
-static uint16_t port = 0;
-
-void load_tracker_info(const char *file, int n)
+struct Tracker
 {
-	int fd = open(file, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	std::string ip;
+	uint16_t port;
+};
+
+// List of trackers
+static std::vector<Tracker> trackers;
+
+void load_tracker_info(const char *file)
+{
+	int fd = open(file, O_RDONLY);
 	if (fd == -1)
 	{
 		std::cerr << "Error opening file" << strerror(errno) << std::endl;
@@ -36,15 +42,42 @@ void load_tracker_info(const char *file, int n)
 	}
 	int i = 1;
 	char *line = contents;
-	while (i <= n)
+	while (true)
 	{
-		char buffer[len];
-		sscanf(line, "%s %hu", buffer, &port);
+		char ip[len];
+		uint16_t port;
+		if (sscanf(line, "%s %hu", ip, &port) != 2)
+		{
+			break;
+		}
 		line = strchr(line, '\n') + 1;
-		ip = buffer;
+		trackers.push_back({ip, port});
+		std::cout << "Tracker " << i << " IP: " << ip << " Port: " << port << std::endl;
 		++i;
 	}
-	std::cout << "IP: " << ip << " Port: " << port << std::endl;
+}
+
+/**
+ * @brief Attempt to connect to any of the trackers
+ */
+bool connect_to_tracker(TCPSocket &tracker)
+{
+	for (auto const &t : trackers)
+	{
+		try
+		{
+			if (tracker.connect(t.ip, t.port))
+			{
+				std::clog << "Connected to tracker " << t.ip << ":" << t.port << std::endl;
+				return true;
+			}
+		}
+		catch (const ConnectionClosedError &e)
+		{
+			std::cerr << t.ip << ":" << t.port << " " << e.what() << std::endl;
+		}
+	}
+	return false;
 }
 
 /**
@@ -52,6 +85,8 @@ void load_tracker_info(const char *file, int n)
  */
 int main(int argc, char *argv[])
 {
+	const char *file_path = nullptr;
+
 	// Parse args
 	if (argc != 3)
 	{
@@ -60,39 +95,49 @@ int main(int argc, char *argv[])
 	}
 	// Ignore argv[1] for now
 
+	file_path = argv[2];
+
 	// Load tracker info
-	load_tracker_info(argv[2], 1);
+	load_tracker_info(file_path);
 
 	// Connect to tracker
 	TCPSocket tracker;
-	try
+	if (!connect_to_tracker(tracker))
 	{
-		tracker.connect(ip, port);
-	}
-	catch (const std::exception &_)
-	{
-		load_tracker_info(argv[2], 2);
-		try
-		{
-			tracker.connect(ip, port);
-		}
-		catch (const NetworkError &e)
-		{
-			std::cerr << e.what() << '\n';
-			exit(1);
-		}
+		std::cerr << "Failed to connect to trackers." << std::endl;
+		exit(1);
 	}
 
-	// Send message to tracker
-	std::string message = "Hello from client!";
-	try
+	// Parse user input
+	while (true)
 	{
-		tracker.send_data(message);
-	}
-	catch (const NetworkError &e)
-	{
-		std::cerr << e.what() << '\n';
-		exit(1);
+		std::string input;
+		std::string response;
+		if (std::cin.peek() != EOF) // check if there is input
+		{
+			std::getline(std::cin >> std::ws, input);
+			if (input == "quit")
+			{
+				break;
+			}
+		}
+		try
+		{
+			tracker.send_data(input);
+			response = tracker.receive_data();
+			std::cout << response << std::endl;
+		}
+		catch (const ConnectionClosedError &e)
+		{
+			std::cerr << e.what() << '\n';
+			std::cerr << "Failed to send or receive data. Reconnecting..." << std::endl;
+			tracker = TCPSocket(); // Create a new socket
+			if (!connect_to_tracker(tracker))
+			{
+				std::cerr << "Failed to reconnect" << std::endl;
+				exit(1);
+			}
+		}
 	}
 
 	return 0;
