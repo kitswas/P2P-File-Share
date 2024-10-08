8e0c2362331ee65c13e2c4b05945e86e676eccc3
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-08 23:38:04 +0530

    Request parser

---
+++ src/tracker/parse_request.cpp
@@ -0,0 +1,50 @@
+#include <sstream>
+
+#include "parse_request.hpp"
+
+std::shared_ptr<Transaction> parse_request(std::shared_ptr<TCPSocket> client, std::string &data)
+{
+	std::stringstream ss(data);
+	std::string request;
+	ss >> request;
+	std::shared_ptr<Transaction> transaction;
+	Request req;
+	if (request == "create_user")
+	{
+		req = UserRequest::CREATE;
+	}
+	else if (request == "delete_user")
+	{
+		req = UserRequest::DELETE;
+	}
+	else if (request == "list_users")
+	{
+		req = UserRequest::LIST;
+	}
+	else if (request == "create_group")
+	{
+		req = GroupRequest::CREATE;
+	}
+	else if (request == "join_group")
+	{
+		req = GroupRequest::JOIN;
+	}
+	else if (request == "leave_group")
+	{
+		req = GroupRequest::LEAVE;
+	}
+	else if (request == "list_groups")
+	{
+		req = GroupRequest::LIST;
+	}
+	else if (request == "list_join_requests")
+	{
+		req = GroupRequest::LIST_JOIN_REQUESTS;
+	}
+	else if (request == "accept_join_request")
+	{
+		req = GroupRequest::ACCEPT_JOIN_REQUEST;
+	}
+	transaction = std::make_shared<Transaction>(req, Endpoint{client->get_peer_ip(), client->get_peer_port()}, data);
+	return transaction;
+}
---
+++ src/tracker/parse_request.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <memory>
+
+#include "../network/tcp_socket.hpp"
+#include "../models/transaction.hpp"
+
+std::shared_ptr<Transaction> parse_request(std::shared_ptr<TCPSocket> client, std::string &data);
