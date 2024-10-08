a6c7bbad52ae3be5bfc536471af9ea2d1be6ab64
kitswas <90329875+kitswas@users.noreply.github.com>
2024-10-08 23:47:57 +0530

    to_string() implementations

--- src/models/endpoint.hpp
+++ src/models/endpoint.hpp
@@ -7,4 +7,14 @@ struct Endpoint
 {
 	std::string ip;
 	uint16_t port;
+	
+	friend bool operator==(const Endpoint &lhs, const Endpoint &rhs) noexcept
+	{
+		return lhs.ip == rhs.ip && lhs.port == rhs.port;
+	}
+	
+	std::string to_string() const
+	{
+		return ip + ":" + std::to_string(port);
+	}
 };
--- src/models/transaction.hpp
+++ src/models/transaction.hpp
@@ -41,4 +41,23 @@ struct Transaction
 	const std::string data;
 
 	Result outcome;
+	
+	std::string to_string() const
+	{
+		return "Transaction: " + std::visit([](auto &&arg) -> std::string {
+			using T = std::decay_t<decltype(arg)>;
+			if constexpr (std::is_same_v<T, UserRequest>)
+			{
+				return "UserRequest";
+			}
+			else if constexpr (std::is_same_v<T, GroupRequest>)
+			{
+				return "GroupRequest";
+			}
+			else // Programmer error, should never happen
+			{
+				static_assert(always_false<T>::value, "non-exhaustive visitor!");
+			}
+		}, request) + " from " + source.to_string() + " with data: " + data;
+	}
 };
